ed662b2dfba636973ba737d4db698696c94a5006
felipefurukawa <felpissss@gmail.com>
2023-10-20 12:40:59 -0300

    primeira impressao

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import pandas as pd
 import pandas_datareader as web
 import datetime as dt
+import yfinance as yf
 
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.python.keras.layers import Dense, Dropout, LSTM
 from tensorflow.python.keras.models import Sequential
+
+yf.pdr_override()
 
 moeda_crypto = 'BTC'
 moeda_comparativa = 'USD'
@@ -14,7 +17,7 @@ moeda_comparativa = 'USD'
 start = dt.datetime(2016,1,1)
 end = dt.datetime.now()
 
-data = web.DataReader(f'{moeda_crypto}-{moeda_comparativa}', 'yahoo', start, end)
+data = yf.download('BTC-USD', start, end)
 
 # Data dataframe
 scaler = MinMaxScaler(feature_range=(0,1))
@@ -32,10 +35,52 @@ x_train, y_train = np.array(x_train), np.array(y_train)
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
 #Criacao rede neural
+print("Shape de x_train:", x_train.shape)
+print("Shape de y_train:", y_train.shape)
 
 model = Sequential()
 
 model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
 model.add(Dropout(0.2))
 model.add(LSTM(units=50, return_sequences=True))
-model.add()
+model.add(Dropout(0.2))
+model.add(LSTM(units=50))
+model.add(Dropout(0.2))
+model.add(Dense(units=1))
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(x_train, y_train, epochs=25, batch_size=32)
+
+
+#Teste da Model
+teste_inicio = dt.datetime(2020,1,1)
+teste_fim = dt.datetime.now()
+
+teste_data = yf.download('BTC-USD', start, end)
+valor_moeda = teste_data['Close'].values
+
+dataset_total = pd.concat((data['Close'], teste_data['Close']), axis=0)
+
+model_inputs = dataset_total[len(dataset_total) - len(teste_data) - dias_predicao:].values
+model_inputs = model_inputs.reshape(-1, 1)
+model_inputs = scaler.fit_transform(model_inputs)
+
+x_teste = []
+
+for x in range(dias_predicao, len(model_inputs)):
+    x_teste.append(model_inputs[x-dias_predicao:x, 0])
+
+x_teste = np.array(x_teste)
+x_teste = np.reshape(x_teste, (x_teste.shape[0], x_teste.shape[1], 1))
+
+precos_predicao = model.predict(x_teste)
+precos_predicao = scaler.inverse_transform(precos_predicao)
+
+plt.plot(valor_moeda, color='black', label='Valores da moeda')
+plt.plot(precos_predicao, color='blue', label='Predição de preços')
+plt.title(f'Predição do valor da {moeda_crypto}')
+plt.xlabel('Time')
+plt.ylabel('Preço')
+plt.legend(loc='upper left')
+plt.show()
+
